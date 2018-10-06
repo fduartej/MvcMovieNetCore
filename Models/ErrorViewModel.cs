4be035bcbb0d1b78411f1a407fe83bb88fa9c95f
Fred <fduartej@gmail.com>
2018-10-06 13:13:52 -0500

    Commit inicial

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MvcMovieNetCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
